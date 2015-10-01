1c3d6dae54f331a12dc3af2bac37da04251d4d59
Wallace <jeremygw@0c4de9a7242a.ant.amazon.com>
2015-10-01 16:15:37 -0700

    more license includes

--- web/app/scripts/main.js
+++ web/app/scripts/main.js
@@ -2,12 +2,20 @@
 * SIMPLE BEER SERVICE | FRONT END CODE
 * This code uses AWS API Gateway to query DynamoDB and get the latest sensor data produced
 * by the Simple Beer Service compute unit.
-*      ___ _       _______
-*     /   | |     / / ___/
-*    / /| | | /| / /\__ \
-*   / ___ | |/ |/ /___/ /
-*  /_/  |_|__/|__//____/
-*
+
+Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+Note: Other license terms may apply to certain, identified software files contained within or
+distributed with the accompanying software if such terms are included in the directory containing
+the accompanying software. Such other license terms will then apply in lieu of the terms of the
+software license above.
+
 * Constants to change
 * -------------------------------------------------------
 * SBS_ENDPOINT: The location of your SBS endpoint.
