f970c6592b80097f8c874787e8517fe7c000db40
Andy <andrfase@a0999b11e339.ant.amazon.com>
2017-08-30 13:31:24 -0700

    1st cut of Ultrasonic code including defining a read function

--- device/components/grove-ultrasonic-ranger.js
+++ device/components/grove-ultrasonic-ranger.js
@@ -1,9 +1,26 @@
-var GroveSensor = require("./grove-sensor.js");
+var five = require("johnny-five");
+var DEFAULT_READ_INTERVAL = 1000;
+
 
 UltrasonicRanger = function(pin, freq) {
-  GroveSensor.call(this, { pin : pin, type: "digital", freq: freq });
+  five.Proximity.call(this, {
+     controller: "HCSR04",
+     pin: pin,
+     freq: freq
+  });
 };
 
-UltrasonicRanger.prototype = Object.create(GroveSensor.prototype, {});
+// UltrasonicRanger.prototype = Object.create(five.Proximity.prototype, {
+//   readInterval: {
+//     value: DEFAULT_READ_INTERVAL
+//   },
+//   read: {
+//     value: function() {
+//       return this.lastProximity;
+//     }
+//   }
+//   });
+
+UltrasonicRanger.prototype.lastProximity = 0;
 
 exports = module.exports = UltrasonicRanger;
